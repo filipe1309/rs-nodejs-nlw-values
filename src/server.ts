359541ada428d96a54fcabaa4de8d4df8bafe317
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-20 21:01:06 -0300

    chore: add routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express()
 
+app.get("/test", (req, res) => res.send("Hello NLW"))
+app.post("/test-post", (req, res) => res.send("Hello NLW Post"))
+
 app.listen(3000, () => {
     console.log("Server is running");
 })
