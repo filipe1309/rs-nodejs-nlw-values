1dd14348cc54071db27c95f84a1d07b55677c3de
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-22 22:34:45 -0300

    chore: handle exceptions with express middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import express from 'express';
 import './database';
+import express, { Request, Response, NextFunction} from 'express';
+import 'express-async-errors';
 import { router } from './routes'
 
 const port = process.env.PORT || 3000;
@@ -8,7 +9,16 @@ const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({error: err.message})
+    }
+
+    return res.status(500).json({status: "error", message: "Internal Server Error"});
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
