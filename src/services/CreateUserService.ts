e29839c16ac700ef17e0debe81f87f3e7d1583cb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-23 23:32:26 -0300

    fix: add default value to user admin field

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -10,7 +10,7 @@ interface IUserRequest {
 }
 
 class CreateUserService {
-    async execute({ name, email, password, admin }: IUserRequest) {
+    async execute({ name, email, password, admin = false }: IUserRequest) {
         if (!email) {
             throw new Error("Email incorrect");
         }
